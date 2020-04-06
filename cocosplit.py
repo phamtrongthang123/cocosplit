a5a0f85179a81db708ef51a52176a65e8e0ccd97
Phạm Trọng Thắng <phamtrongthang123@gmail.com>
2020-04-06 20:15:55 +0700

    Update cocosplit.py

--- cocosplit.py
+++ cocosplit.py
@@ -15,9 +15,9 @@ parser.add_argument('--having-annotations', dest='having_annotations', action='s
 
 args = parser.parse_args()
 
-def save_coco(file, info, licenses, images, annotations, categories):
+def save_coco(file, images, annotations, categories):
     with open(file, 'wt', encoding='UTF-8') as coco:
-        json.dump({ 'info': info, 'licenses': licenses, 'images': images, 
+        json.dump({ 'images': images, 
             'annotations': annotations, 'categories': categories}, coco, indent=2, sort_keys=True)
 
 def filter_annotations(annotations, images):
@@ -27,23 +27,23 @@ def filter_annotations(annotations, images):
 def main(args):
     with open(args.annotations, 'rt', encoding='UTF-8') as annotations:
         coco = json.load(annotations)
-        info = coco['info']
-        licenses = coco['licenses']
+        # info = coco['info']
+        # licenses = coco['licenses']
         images = coco['images']
         annotations = coco['annotations']
         categories = coco['categories']
 
         number_of_images = len(images)
-
-        images_with_annotations = funcy.lmap(lambda a: int(a['image_id']), annotations)
-
+        print(number_of_images)
+        images_with_annotations = funcy.lmap(lambda a: a['image_id'], annotations)
+        print(images_with_annotations)
         if args.having_annotations:
             images = funcy.lremove(lambda i: i['id'] not in images_with_annotations, images)
-
+        print(images)
         x, y = train_test_split(images, train_size=args.split)
 
-        save_coco(args.train, info, licenses, x, filter_annotations(annotations, x), categories)
-        save_coco(args.test, info, licenses, y, filter_annotations(annotations, y), categories)
+        save_coco(args.train,  x, filter_annotations(annotations, x), categories)
+        save_coco(args.test, y, filter_annotations(annotations, y), categories)
 
         print("Saved {} entries in {} and {} in {}".format(len(x), args.train, len(y), args.test))
 
